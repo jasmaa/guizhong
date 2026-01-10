46f9cd95a032927f1856e8c81b7307cb9523c11c
jasmaa <jasonmaa3955@gmail.com>
2026-01-10 12:26:53 -0500

    Switch to download music from yt-dlp

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import subprocess
+import hashlib
+from pathlib import Path
 import asyncio
 import discord
 from discord.ext import commands
-from mutagen.mp3 import MP3
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,34 +17,32 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=discord_command_prefix, intents=intents)
 session_cache = {}
-BASE_PATH = "music"
+music_cache_path = Path("/tmp/guizhong/music_cache")
 
-async def play_loop(voicechannel):
-    while True:
-        if voicechannel.id in session_cache:
-            vc = session_cache[voicechannel.id]["vc"]
-            filename = session_cache[voicechannel.id]["queue"].pop(0)
-            session_cache[voicechannel.id]["queue"].append(filename)
-            session_cache[voicechannel.id]["current_song"] = filename
 
-            p = os.path.join(BASE_PATH, filename)
-            f = MP3(p)
+class Session:
+    def __init__(self, vc):
+        self.vc = vc
+        self.queue = []
+        self.play_music_task = None
 
-            vc.stop()
-            vc.play(discord.FFmpegPCMAudio(p), after=lambda e: print(
-                f'Player error: {e}') if e else None)
 
-            async def cancellable_sleep():
-                try:
-                    await asyncio.sleep(f.info.length)
-                except asyncio.CancelledError:
-                    print("sleep cancelled")
+class Song:
+    def __init__(self, name, filepath):
+        self.filepath = filepath
 
-            session_cache[voicechannel.id]["sleep_task"] = asyncio.create_task(
-                cancellable_sleep())
-            await session_cache[voicechannel.id]["sleep_task"]
-        else:
-            break
+
+def get_or_download_youtube_mp3(voicechannel_id, url):
+    h = hashlib.md5(url.encode("utf8")).hexdigest()
+    filename = f"{h}.mp3"
+
+    parent_path = music_cache_path / str(voicechannel_id)
+    filepath = parent_path / filename
+
+    if not os.path.exists(filepath):
+        subprocess.run(["yt-dlp", "-x", "--audio-format", "mp3", "-P", parent_path, "-o", filename, url])
+    
+    return filepath
 
 
 @bot.event
@@ -61,43 +61,111 @@ async def get_voicechannel(ctx):
     return voicechannel
 
 
+async def play_queue(voicechannel_id):
+    session = session_cache[voicechannel_id]
+    vc = session.vc
+    queue = session.queue
+
+    # Play next song in the queue
+    if len(queue) > 0:
+        filepath = queue[0].filepath
+
+        def post_play(e):
+            queue.pop()
+            session.play_loop_task = asyncio.create_task(play_queue(voicechannel_id))
+        
+        vc.stop()
+        vc.play(discord.FFmpegPCMAudio(filepath), after=post_play)
+    else:
+        vc.stop()
+        session.play_loop_task = None
+
+
 @bot.command()
 async def info(ctx):
     voicechannel = await get_voicechannel(ctx)
     if voicechannel.id in session_cache:
-        q = session_cache[voicechannel.id]["queue"]
-        current_song = session_cache[voicechannel.id]["current_song"]
-        await ctx.send("```\n" + f"Now playing: {current_song}\n\n" + "Queue:\n" + "\n".join([f'{i+1}: {f}' for i, f in enumerate(q)][:5]) + "\n```")
+        session = session_cache[voicechannel.id]
+        queue = session.queue
+
+        if len(queue) > 0:
+            current_song = queue[0]
+            await ctx.send("```\n" + f"Now playing: {current_song.filepath}\n\n" + "Queue:\n" + "\n".join([f'{i+1}: {song.filepath}' for i, song in enumerate(queue)][:5]) + "\n```")
+        else:
+            await ctx.send("```\nNot playing\n```")
     else:
         await ctx.send("```\nNot playing...\n```")
 
 
 @bot.command()
-async def play(ctx):
+async def play(ctx, arg):
     voicechannel = await get_voicechannel(ctx)
+
+    # Connect or get voice client
     if voicechannel.id not in session_cache:
         vc = await voicechannel.connect()
-        session_cache[voicechannel.id] = {
-            "vc": vc,
-            "queue": [f for f in os.listdir("music") if f.endswith("mp3")],
-            "current_song": None,
-            "sleep_task": None,
-            "play_loop_task": asyncio.create_task(play_loop(voicechannel)),
-        }
-    if session_cache[voicechannel.id]["sleep_task"]:
-        session_cache[voicechannel.id]["sleep_task"].cancel()
+        session_cache[voicechannel.id] = Session(vc=vc)
+    
+    session = session_cache[voicechannel.id]
+    vc = session.vc
+    queue = session.queue
+    
+    # Download or get music
+    print("Downloading...")
+    url = arg
+    filepath = get_or_download_youtube_mp3(voicechannel.id, url)
+    print(f"Downloaded to {filepath}")
+
+    # TODO: get song name
+    song = Song(name=filepath, filepath=filepath)
+
+    queue.append(song)
+    print(f"Added {song} to queue")
+
+    if not vc.is_playing():
+        session.play_loop_task = asyncio.create_task(play_queue(voicechannel.id))
+
+
+@bot.command()
+async def pause(ctx):
+    voicechannel = await get_voicechannel(ctx)
+
+    if voicechannel.id not in session_cache:
+        ctx.send("Not in the voice channel")
+        return
+    
+    session = session_cache[voicechannel.id]
+    vc = session.vc
+
+    vc.pause()
+
+
+@bot.command()
+async def resume(ctx):
+    voicechannel = await get_voicechannel(ctx)
+
+    if voicechannel.id not in session_cache:
+        ctx.send("Not in the voice channel")
+        return
+    
+    session = session_cache[voicechannel.id]
+    vc = session.vc
+
+    vc.resume()
 
 
 @bot.command()
 async def stop(ctx):
     voicechannel = await get_voicechannel(ctx)
     if voicechannel.id in session_cache:
-        vc = session_cache[voicechannel.id]["vc"]
+        session = session_cache[voicechannel.id]
+        vc = session.vc
+        play_loop_task = session.play_loop_task
+
         vc.stop()
         await vc.disconnect()
-        if session_cache[voicechannel.id]["sleep_task"]:
-            session_cache[voicechannel.id]["sleep_task"].cancel()
-        session_cache[voicechannel.id]["play_loop_task"].cancel()
+        if play_loop_task is not None:
+            play_loop_task.cancel()
         del session_cache[voicechannel.id]
 
 bot.run(discord_token)
